6bcbb321bb7dace97463bc8120368f824b11642b
Soeng Souy <soengsouy@gmail.com>
2023-01-19 19:38:07 +0700

    add all error redirect page error

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -47,4 +47,19 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    /**
+     * Render the exception into an HTTP response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function render($request, Throwable $exception)
+    {
+        if($this->isHttpException($exception)) {
+            return response()->view('errors.404');
+        } else {
+            return response()->view('errors.500');
+        }
+    }
 }
